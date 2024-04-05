e1bbcf1aaec17e27228288c125f7b8f89cd2b63f
Nur0606 <seidekmatov@gmail.com>
2024-04-05 14:28:41 +0600

    pole,constructor,get,set,toString koshtum

--- src/model/Announcement.java
+++ src/model/Announcement.java
@@ -1,5 +1,82 @@
 package model;
 
 public class Announcement {
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private String owner;
+    private Favorite favorite;
 
+    public Announcement() {
+    }
+
+    public Announcement(Long id, String name, String description, int price, String owner, Favorite favorite) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.owner = owner;
+        this.favorite = favorite;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Favorite getFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(Favorite favorite) {
+        this.favorite = favorite;
+    }
+
+    @Override
+    public String toString() {
+        return "Announcement{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", description='" + description + '\'' +
+               ", price=" + price +
+               ", owner='" + owner + '\'' +
+               ", favorite=" + favorite +
+               '}';
+    }
 }
