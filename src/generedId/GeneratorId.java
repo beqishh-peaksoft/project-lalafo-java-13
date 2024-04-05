7807814d825f00ab561c8397f9a9ef92e6d1e1f5
eldiuar <avazbekuulualdiar326@gmail.com>
2024-04-05 14:40:41 +0300

    added generator id

--- src/generedId/GeneratorId.java
+++ src/generedId/GeneratorId.java
@@ -5,14 +5,15 @@ public class GeneratorId {
     public static Long favoriteId = 0L;
     public static Long userId = 0L;
 
-    public static Long idAnnouncementId() {
+    public static Long genAnnouncementId() {
         return ++announcementId;
     }
-    public static Long idFavorite() {
+
+    public static Long genFavoriteId() {
         return ++favoriteId;
     }
-    public static Long idUser() {
+
+    public static Long genUserId() {
         return ++userId;
     }
-
 }
